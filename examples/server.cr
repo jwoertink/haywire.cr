3f48dbb4f238f1caa283cdcd1ab3547cab2ce081
Jeremy Woertink <jeremywoertink@gmail.com>
2017-05-11 12:41:49 -0700

    looks like all config options are required to boot this thing

--- examples/server.cr
+++ examples/server.cr
@@ -4,7 +4,12 @@ require "../src/haywire"
 config = LibHaywire::Config.new
 config.http_listen_address = "0.0.0.0"
 config.http_listen_port = 3000
-config.thread_count = 1
+config.thread_count = 0
+config.balancer = "ipc"
+config.parser = "http_parser"
+config.max_request_size = 1048576
+config.tcp_nodelay = true
+config.listen_backlog = 0
 
 # This throws a Invalid memory access error :(
 LibHaywire.init_with_config(pointerof(config))
