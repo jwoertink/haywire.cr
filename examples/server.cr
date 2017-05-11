54359c0abc65674856e3b991efdb5411d9238b6d
Jeremy Woertink <jeremywoertink@gmail.com>
2017-05-11 14:06:36 -0700

    updated example with proper response

--- examples/server.cr
+++ examples/server.cr
@@ -13,8 +13,18 @@ config.listen_backlog = 0
 
 # This throws a Invalid memory access error :(
 LibHaywire.init_with_config(pointerof(config))
-LibHaywire.http_add_route("/", -> {
-  "Hello World"
+LibHaywire.http_add_route("/", ->(request : LibHaywire::HTTP_Request, response : LibHaywire::HTTP_Response, user_data : Void*) {
+  status_code = LibHaywire::HW_String.new(value: LibHaywire::HTTP_STATUS_200, length: 6)
+  LibHaywire.set_response_status_code(pointerof(response), pointerof(status_code))
+
+  content_type_name = LibHaywire::HW_String.new(value: "Content-Type", length: 12)
+  content_type_value = LibHaywire::HW_String.new(value: "text/html", length: 9)
+  LibHaywire.set_response_header(pointerof(response), pointerof(content_type_name), pointerof(content_type_value))
+
+  body = LibHaywire::HW_String.new(value: "Hello World", length: 11)
+  LibHaywire.set_body(pointerof(response), pointerof(body))
+
+  LibHaywire.http_response_send(pointerof(response), "user_data", ->(request : LibHaywire::HTTP_Request, response : LibHaywire::HTTP_Response, user_data : Void*) {})
 }, nil)
 LibHaywire.http_open
 
